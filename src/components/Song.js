59faaa71011c29dba1c58ff7c87f90b8b6eb2736
Marta <martadabrowka@gmail.com>
2018-01-16 22:25:33 +0000

    changes to Song

--- src/components/Song.js
+++ src/components/Song.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 class Song extends React.Component {
   render(){
-    if (!this.props.song) return null;
+    // if (!this.props.song) return null;
 
     return (
       <div>
-
+        <h3>{this.props.position}</h3>
+       <h4>Artist: {this.props.artist}</h4>
+       <h5>Title: {this.props.title}</h5>
     </div>
     );
   }
