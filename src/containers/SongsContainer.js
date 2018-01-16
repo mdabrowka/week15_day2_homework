e75c2f3f217eb3a481b092af3ae592fbf6628ecd
Marta <martadabrowka@gmail.com>
2018-01-16 20:43:19 +0000

    added render function to SongsContainer

---
+++ src/containers/SongsContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SongDisplay from '../components/SongDisplay.js';
+
+class SongsContainer extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h2>ITunes UK Chart Hits</h2> 
+      </div>
+    )
+  }
+
+
+}
+
+
+
+export default SongsContainer;
