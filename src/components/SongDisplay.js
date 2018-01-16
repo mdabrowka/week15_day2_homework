7da595f7411fb0161fe3ae065ff03b2060560aeb
Marta <martadabrowka@gmail.com>
2018-01-16 21:00:29 +0000

    successful api request

---
+++ src/components/SongDisplay.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SongDisplay extends React.Component {
+
+render() {
+  return (
+    <div></div>
+
+    )
+}
+
+
+
+}
+
+
+export default SongDisplay;
--- src/containers/SongsContainer.js
+++ src/containers/SongsContainer.js
@@ -2,16 +2,37 @@ import React from 'react';
 import SongDisplay from '../components/SongDisplay.js';
 
 class SongsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      songs: []
+    }
+  }
+
+  componentDidMount() {
+    const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.send();
+
+      request.addEventListener('load', () => {
+      if(request.status !== 200) return;
+      const jsonString = request.responseText;
+      const songs = JSON.parse(jsonString);
+      console.log(songs);
+      this.setState({songs : songs });
+
+  });
+  }
 
   render() {
     return (
       <div>
-        <h2>ITunes UK Chart Hits</h2> 
+        <h2>ITunes UK Chart Hits</h2>
+       <SongDisplay songs={this.state.songs}/>
       </div>
-    )
+    );
   }
-
-
 }
 
 
